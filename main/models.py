49e63e87e270488b1c38d81f41dcdfc7ca7f1dc7
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2023-09-16 20:37:02 +0530

    model commit

--- main/models.py
+++ main/models.py
@@ -113,6 +113,86 @@ class Cart(models.Model):
 
 
 # OrderItems
+class CartItems(models.Model):
+    item=models.CharField(max_length=150)
+    image=models.CharField(max_length=200)
+    qty=models.IntegerField()
+    price=models.FloatField()
+    total=models.FloatField()
 
+    def image_tag(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" />' % (self.image))
+
+
+# Order
+status_choice=(
+        ('process','In Process'),
+        ('shipped','Shipped'),
+        ('delivered','Delivered'),
+    )
+class CartOrder(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    total_amt=models.FloatField()
+    paid_status=models.BooleanField(default=False)
+    order_dt=models.DateTimeField(auto_now_add=True)
+    order_status=models.CharField(choices=status_choice,default='process',max_length=150)
+
+    class Meta:
+        verbose_name_plural='8. Orders'
+
+# OrderItems
+class CartOrderItems(models.Model):
+    order=models.ForeignKey(CartOrder,on_delete=models.CASCADE)
+    invoice_no=models.CharField(max_length=150)
+    item=models.CharField(max_length=150)
+    image=models.CharField(max_length=200)
+    qty=models.IntegerField()
+    price=models.FloatField()
+    total=models.FloatField()
+    
+
+    class Meta:
+        verbose_name_plural='9. Order Items'
+
+    def image_tag(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" />' % (self.image))
+
+# Product Review
+RATING=(
+    (1,'1'),
+    (2,'2'),
+    (3,'3'),
+    (4,'4'),
+    (5,'5'),
+)
+class ProductReview(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    review_text=models.TextField()
+    review_rating=models.CharField(choices=RATING,max_length=150)
+
+    class Meta:
+        verbose_name_plural='Reviews'
+
+    def get_review_rating(self):
+        return self.review_rating
+
+# WishList
+class Wishlist(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural='Wishlist'
+
+# AddressBook
+class UserAddressBook(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    mobile=models.CharField(max_length=50,null=True)
+    address=models.TextField()
+    status=models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural='AddressBook'
 
     
