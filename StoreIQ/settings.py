795318a5e8ea4ffff29384a45a33fe3117163c66
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2023-09-16 20:34:23 +0530

    second commit

--- StoreIQ/settings.py
+++ StoreIQ/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,9 +119,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# PAYPAL_RECEIVER_EMAIL = 'sb-c5xgx6555500@business.example.com'
+# PAYPAL_TEST = True
+
+# KEY = 'rzp_test_oYyfDYq5uOcOKz'
+# SECRET = 'Ozy5O0ak4Wz7ivjRIJQlJRUz'
+
+JET_SIDE_MENU_COMPACT = True
--- StoreIQ/urls.py
+++ StoreIQ/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('jet/', include('jet.urls')),
+    path('jet/dashboard/', include('jet.dashboard.urls','jet-dashboard')),
     path('admin/', admin.site.urls),
+    path('',include('main.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
