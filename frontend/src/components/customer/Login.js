08b44bff63129fcfee65e19704a07a1e8397f73b
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2024-02-10 11:19:28 +0530

    commit

--- frontend/src/components/customer/Login.js
+++ frontend/src/components/customer/Login.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from '../../logo.svg';
 
 function Login(props){
+    const [loginFormData, setLoginFormData] = useState({
+        username: "",
+        password: ""
+    });
+
+    const inputHandler = (e) => {
+        setLoginFormData({
+            ...loginFormData,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const submitHandler = (e) => {
+        // e.preventDefault();
+        const formData = new FormData();
+        formData.append("username", loginFormData.username);
+        formData.append("password", loginFormData.password);
+        console.log(formData);
+    };
+
+    // console.log(loginFormData);
+
     return(
         <div className="container mt-4">
             {/* <h3 className="mb-4">Register</h3> */}
@@ -14,13 +36,13 @@ function Login(props){
                             <form>
                                 <div className="mb-3">
                                     <label for="username" className="form-label">Username</label>
-                                    <input type="text" className="form-control" id="username"/>
+                                    <input type="text" name="username" value={loginFormData.username} onChange={inputHandler} className="form-control" id="username"/>
                                 </div>
                                 <div className="mb-3">
                                     <label for="pwd" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="pwd"/>
+                                    <input type="password" name="password" value={loginFormData.password} onChange={inputHandler} className="form-control" id="pwd"/>
                                 </div>
-                                <button type="submit" className="btn btn-primary">Submit</button>
+                                <button type="button" onClick={submitHandler} className="btn btn-primary">Submit</button>
                             </form>
                         </div>
                     </div>
